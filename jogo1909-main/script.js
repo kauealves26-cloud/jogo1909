6a1c9b16d87cd85fd245958b7979be528478c02f
kauealves26-cloud <kaue.alves26@escola.pr.gov.br>
2025-10-03 14:23:48 -0300

    salvar

---
+++ jogo1909-main/script.js
@@ -0,0 +1,11 @@
+const avanca = document.querySelector;
+
+avanca.forEach(button => {
+     button.addEventListener(cancelIdleCallback, function('ativo'){
+      const proximoPasso = 'passo-' + this.GEtaTTRIBUTE('data-proximo')
+     
+    
+     atual.classList.remove('ativo');
+      document.getELemntById(proximoPasso).classList.add('ativo');  
+    })
+})
